a41e85910ed0a933ccafe72c545c3b4523aa30c1
Ayuhersi <ayub7hersi@gmail.com>
2022-02-10 01:15:50 +0300

    updating quote-forms

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = new Quote(0, '', '', '', '');
+  @Output() addQuote = new EventEmitter<Quote>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitAuthor() {
+    this.addQuote.emit(this.newQuote);
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
