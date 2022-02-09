b7593625d9abd7be88365277b8447bcb937c5c16
Ayuhersi <ayub7hersi@gmail.com>
2022-02-10 01:11:20 +0300

    adding quote details

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  OnInit,
+  Input,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.css']
+  styleUrls: ['./quote-details.component.css'],
 })
 export class QuoteDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.vote = 0;
   }
 
+  @Input()
+  quotes!: Quote;
+  vote = 0;
+  @Output() isComplete = new EventEmitter<boolean>();
+  quoteComplete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+  numberOfVotes: number = 0;
+
+  thumbsUpButtonClick() {
+    this.numberOfVotes++;
+  }
+  thumbsDownButtonClick() {
+    this.numberOfVotes--;
+  }
+
+  ngOnInit() {}
 }
